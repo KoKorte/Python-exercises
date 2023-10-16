7c73e284e5a313e7549ffc06f365597ef1b41ef9
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-16 13:21:50 +0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,89 @@
+#
+# Tehtävä 101-1: Kirjoita ohjelma, joka tulostaa oman nimesi.
+#
+print("\nTASK 1")
+print("Niko Korteniemi")
+
+#
+# Tehtävä 101-2: Kirjoita ohjelma, joka tulostaa seuraavat nimet aakkosjärjestyksessä:
+# "Aarne", "Bertta", "Celsius", "Daavid"
+#
+print("\nTASK 2")
+print("Aarne")
+print("Bertta")
+print("Celsius")
+print("Daavid")
+
+#
+# Tehtävä 101-3: Kirjoita ohjelma, jossa on nimi "Eemeli" viimeisenä ja "Aapeli" Aarnen jälkeen
+# Protip: SHIFT + nuolinäppäimillä on helppo maalata tekstiä
+# CTRL+C kopioi tekstin
+# CTRL-V tallettaa kopioidun tekstin näytölle
+# CTRL-Z on undo eli se poistaa viimeisen talletuksen näytölle
+#
+print("\nTASK 3")
+print("Aarne")
+print("Aapeli")
+print("Bertta")
+print("Celsius")
+print("Daavid")
+print("Eemeli")
+
+#
+# Tehtävä 101-4: Kirjoita ohjelma, joka kirjoittaa seuraavan riimin:
+# Mä olen olen kotoisin
+# Hyvinkään getosta.
+# Jos joku muuta väittää
+# se on petosta!
+#
+print("\nTASK 4")
+print("Mä olen olen kotoisin")
+print("Hyvinkän getosta.")
+print("Jos joku muuta väittää")
+print("se on petosta!")
+
+"""
+Laskutehtävät
+=============
+
+Perusaritmetiikan merkit ovat + - / ja *
+
+print(1+2)
+print(1*2+3)
+"""
+#
+#Tehtävä 101-5: Kirjoita ohjelma, joka laskee vuoden tuntien lukumäärän
+#
+print("\nTASK 5")
+print("Vuodessa on", 365*24,"tuntia")
+print("Karkaus vuodessa on", 366*24,"tuntia")
+
+#
+#Tehtävä 101-6: Kirjoita ohjelma, joka laskee vuoden minuuttien lukumäärän
+#
+print("\nTASK 6")
+print("Vuodessa on", 365*1440,"minuuttia")
+print("Karkaus vuodessa on", 366*1440,"minuuttia")
+
+
+"""
+Bonus
+=====
+
+Tulostuksessa käytetään joko '' tai "" merkkejä. 
+Näitä merkkejä voi käyttää yhdessä, kunhan niiden keskinäinen sisäkkäinen järjestys on sama.
+"""
+#
+#Tehtävä 101-7: Kirjoita ohjelma, joka tulostaa
+#
+#Terve, nimeni on Simo 'Sapeli' Suomalainen!
+#
+print("\nTASK 7")
+print("Terve, nimeni on Simo 'Sapeli' Suomelainen!")
+
+#
+#Tehtävä 101-8: Kirjoita ohjelma, joka tulostaa
+#print("Hellow world!")
+#
+print("\nTASK 8")
+print('print("Hellow world!")')

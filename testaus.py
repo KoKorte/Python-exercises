e34d2034458d130667061bd38c7947cec6106a5b
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-16 10:20:39 +0300

    Update testaus.py

--- testaus.py
+++ testaus.py
@@ -1,10 +1,15 @@
 vartest = 7
-
-
-
 print("Hello World!")
 #one line comment test
 """
 multi line comment test
 """
-print("Let's print vartest", vartest)
+print("Let's print vartest variable", vartest)
+
+#multi line test
+print("""
+Line
+test
+"""
+)
+print("""\nLine\ntest\n""")

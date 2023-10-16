09d94af519564aca70fbdcf83e3b79aa6be55a34
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-16 10:35:14 +0300

    Update testaus.py

--- testaus.py
+++ testaus.py
@@ -1,15 +1,33 @@
+#variable for vartest
 vartest = 7
+#simple hello world test
 print("Hello World!")
+
 #one line comment test
 """
 multi line comment test
 """
 print("Let's print vartest variable", vartest)
 
+#
 #multi line test
+#
 print("""
 Line
 test
 """
 )
 print("""\nLine\ntest\n""")
+
+#
+#separator test
+#
+print("Hello","World!", sep=" ")
+
+#
+#list test
+#
+mylistA = ["Minä", "olen", "Niko"]
+
+print(mylistA)
+print(type(mylistA))

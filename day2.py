decb27e5754c004cdf23aefcc9b0c95b485f311b
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-17 11:22:29 +0300

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,1 +1,18 @@
+print("\n#################\n")
+print("START")
+print("\n#################\n")
+#
+#1: Kirjoita ohjelma, joka kysyy oman nimesi ja tulostaa sen.
+#
+print("TASK 1.")
+name = input("Please, enter your name: ")
+print("Hello, ", name)
+
+print("\n#################\n")
+#
+#2: Kirjoita ohjelma, joka kysyy oman nimesi kerran ja tulostaa sen kahdesti.
+#
+print("TASK 2.")
+namename = input("Please, enter your name: ")
+print("Hello, ",namename *2)
 

f69a24c9313b8d20c23b22000a7ec2ab8eb280c6
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-17 11:51:58 +0300

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,46 @@
+"""
+PYTHON 103
+==========
+
+Tavoitteet:
+-Ymmärtää erilaiset muuttujat
+-Ymmärtää tietotyypit eli millaisia asioita voi tallettaa erilaisiin muuttujiin
+-Ymmärtää mitä tarkoittaa string, integer ja floating point number
+
+KERTAUS
+
+Kysytään miten muuttujat, input ja print toimivat...
+
+Muuttujat eivät ole välttämättä aina merkkijonoja vaan ne voivat olla numeroita
+
+luku1 = 100
+luku2 = "100"
+
+print(luku1)
+print(luku2)
+
+f merkkijonolla tulostus
+
+tulos = 111 * 2
+print(f"Tulos: {tulos}")
+
+Tehtävä 103-1: Kirjoita ohjelma, joka tulostaa seuraavan f-merkkijonoilla
+Hei olen etunimi sukunimi ja olen X-vuotias.
+
+Liukulukujen (float) toiminta
+
+luku1 = 1.23
+luku2 = -2.34
+luku3 = 3.45
+
+keskiarvo = (luku1 + luku2 + luku3) / 3
+print(f"Keskiarvo: {keskiarvo}")
+
+Tehtävä 103-1: Kirjoita ohjelma, jossa on kaksi muuttujaa x = 123 ja y = 456
+
+Se tulostaa
+x + y =
+x - y =
+x * y =
+x / y =
+"""

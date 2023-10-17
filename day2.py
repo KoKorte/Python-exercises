e8b01eb32c8cf3c1ac87eeae2386b3cf3073aa80
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-17 11:27:02 +0300

    Update day2.py

--- day2.py
+++ day2.py
@@ -16,3 +16,37 @@ print("TASK 2.")
 namename = input("Please, enter your name: ")
 print("Hello, ",namename *2)
 
+print("\n#################\n")
+#
+#3: Kirjoita ohjelma, joka kysyy kaksi kertaa nimen ja tulostaa sitten annetun nimen.
+#
+print("TASK 3.")
+nameone = input("Please, enter your name: ")
+nametwo = input("Please, repeat that: ")
+
+print(nameone + nametwo)
+
+print("\n#################\n")
+#
+#4: Kirjoita ohjelma, joka kysyy nimen ja tulostaa jonkin erikoismerkin ja nimen ja toisen erikoismerkin.
+#Esimerkiksi: !Aapeli@
+#
+print("TASK 4.")
+
+print("\n#################\n")
+#
+#5: Kirjoita ohjelma, joka kysyy etunimen ja sukunimen ja kirjoittaa "nimesi on <etunimi> sukunimi>.
+#
+print("TASK 5.")
+
+print("\n#################\n")
+#
+#6: Kirjoita ohjelma, joka kysyy nimesi ja osoitteesi ja tulostaa sen kuten osoite kirjoitetaan kirjeessä.
+#
+print("TASK 6.")
+
+print("\n#################\n")
+#
+#7: Kirjoita ohjelma, joka kysyy 2-3 asiaa ja tulostaa lyhyen muutaman rivin tarinan, jossa näitä asioita on käytetty.
+#
+print("BONUS TASK 7.")

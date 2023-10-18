73c64519453eb60d3b27da49c6016f99de7652ba
Niko Korteniemi <102418734+KoKorte@users.noreply.github.com>
2023-10-18 11:06:54 +0300

    Create NumberQuessing.py

---
+++ NumberQuessing.py
@@ -0,0 +1,17 @@
+#NUMBER QUESSING GAME
+
+#REQUIREMENTS:
+"""
+-quess a number between 1-100 (range of accepted inputs)
+-print(random.randrange(1,101))
+-to win the number has to be same as a secret number
+-import random
+-10 wrong answers and the game is lost
+-restart after game ends (RESTART? Y\N: )?
+"""
+
+import random
+
+
+
+print(random.randrange(1,101))
